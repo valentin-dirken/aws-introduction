281752aad3cd93a0766f8b492f668317571e0c04
Valentin Dirken <3280182+valentin-dirken@users.noreply.github.com>
2024-02-24 14:38:13 +0100

    Object of type Decimal is not JSON serializable

--- 01_GET_dynamoDB_read.py
+++ 01_GET_dynamoDB_read.py
@@ -1,6 +1,17 @@
 import json
 import boto3
 
+# <Fix> when the DynamoDB field is Decimal : Object of type Decimal is not JSON serializable
+from decimal import *
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return json.JSONEncoder.default(self, obj)
+# </Fix>
+
+
+
 dynamodb = boto3.resource('dynamodb')
 table_name = 'vdirken_users'
 table = dynamodb.Table(table_name)
@@ -11,6 +22,5 @@ def lambda_handler(event, context):
     items = response['Items']
     return {
                     'statusCode': 200,
-                    'body': json.dumps({'message': 'Items retrieved successfully', 'items': items})
+                    'body': json.dumps({'message': 'Items retrieved successfully', 'items': items}, cls=JSONEncoder)
             }
-    
