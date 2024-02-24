b3a47e35cc08aa71f5ee10114877662d65d5b4ed
Valentin Dirken <3280182+valentin-dirken@users.noreply.github.com>
2024-02-24 14:26:44 +0100

    Create dynamoDB_read_with_params.py

---
+++ dynamoDB_read_with_params.py
@@ -0,0 +1,48 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table_name = 'ppp_clients_table'
+table = dynamodb.Table(table_name)
+
+def lambda_handler(event, context):
+
+    try:
+        
+        # Retrieve the item ID to read from the query parameters
+        clientsID = event.get("params", {}).get("clientsID")
+        print(clientsID)
+
+        # If an ID is provided, read the specific item
+        if clientsID:
+            response = table.get_item(Key={'clientsID': clientsID})
+            if 'Item' in response:
+                item = response['Item']
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps({'message': 'Item retrieved successfully', 'item': item})
+                }
+            else:
+                return {
+                    'statusCode': 404,
+                    'body': json.dumps({'message': 'Item not found'})
+                }
+        else:
+            # Retrieve all items from the DynamoDB table
+            response = table.scan()
+            if 'Items' in response:
+                items = response['Items']
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps({'message': 'Items retrieved successfully', 'items': items})
+                }
+            else:
+                return {
+                    'statusCode': 404,
+                    'body': json.dumps({'message': 'No items found'})
+                }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': 'An error occurred', 'error': str(e)})
+        }
