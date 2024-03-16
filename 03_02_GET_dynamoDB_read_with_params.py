e9e78d437f2f13397b28bb211604fc3c23e4fe16
Valentin Dirken <3280182+valentin-dirken@users.noreply.github.com>
2024-03-16 12:01:34 +0100

    Update 03_02_GET_dynamoDB_read_with_params.py
    Fix - Decimal Issue

--- 03_02_GET_dynamoDB_read_with_params.py
+++ 03_02_GET_dynamoDB_read_with_params.py
@@ -1,6 +1,15 @@
 import json
 import boto3
 
+# <Fix> when the DynamoDB field is Decimal : Object of type Decimal is not JSON serializable
+from decimal import *
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return json.JSONEncoder.default(self, obj)
+# </Fix>
+
 dynamodb = boto3.resource('dynamodb')
 table_name = 'ppp_clients_table'
 table = dynamodb.Table(table_name)
@@ -34,7 +43,7 @@ def lambda_handler(event, context):
                 items = response['Items']
                 return {
                     'statusCode': 200,
-                    'body': json.dumps({'message': 'Items retrieved successfully', 'items': items})
+                    'body': json.dumps({'message': 'Items retrieved successfully', 'items': items}, cls=JSONEncoder)
                 }
             else:
                 return {
