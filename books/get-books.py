dd8c18cb4d0e8cad4b26c91b947ce1f020f9e814
Valentin Dirken <3280182+valentin-dirken@users.noreply.github.com>
2024-05-17 16:41:47 +0200

    Create get-books.py

---
+++ books/get-books.py
@@ -0,0 +1,26 @@
+import json
+import boto3
+
+# <Fix> when the DynamoDB field is Decimal : Object of type Decimal is not JSON serializable
+from decimal import *
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return json.JSONEncoder.default(self, obj)
+# </Fix>
+
+
+
+dynamodb = boto3.resource('dynamodb')
+table_name = 'vdirken-books'
+table = dynamodb.Table(table_name)
+
+def lambda_handler(event, context):
+    
+    response = table.scan()
+    items = response['Items']
+    return {
+                    'statusCode': 200,
+                    'body': json.dumps({'message': 'Items retrieved successfully', 'items': items}, cls=JSONEncoder)
+            }
